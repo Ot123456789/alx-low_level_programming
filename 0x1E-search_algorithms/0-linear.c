9047b5d6c6779781bb76e07005ae7111ce6b5a3e
Ot123456789 <otmaneelouafi@gmail.com>
2024-05-08 16:22:45 -0700

    up

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,17 +13,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
-
 	if (array == NULL)
-		return (-1); /* Return -1 if the array is NULL */
+		return -1; /* Return -1 if the array is NULL */
 
-	for (i = 0; i < size; ++i)
+	for (size_i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-			return (i); /* Return the index if the value is found */
+			return i; /* Return the index if the value is found */
 	}
-	return (-1); /* Return -1 if the value is not found in the array */
+	return -1; /* Return -1 if the value is not found in the array */
 }
